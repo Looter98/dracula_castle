bfe9d8ec28b32526e3433f9b5f288184c82708b9
Looter98 <35749173+Looter98@users.noreply.github.com>
2020-10-20 11:46:30 +1300

    Basic room linking mechanic (WIP)

--- room.py
+++ room.py
@@ -1,22 +1,33 @@
 class Rooms:
     room_name = ""
     room_desc = ""
-    room_player = 0 #0 is no, 1 is yes
+    room_position = []
     room_links = []
-    #room_chest =
+    room_player = []
+    #room_chest = #Calls a chest to appear in the room 
+    #room_fight = #Calls a fight to play out when you enter the room
     
 
     def __init__ (room_name, room_desc):
         self._room_name = room_name
         self._room_desc = room_desc
+        self._room_position = room_position
+        self._room_links = room_links
 
+    def player_travel():
+        if("N" in self._player_room and "S" in self._player_room + [0,1] ):
+            pass
 
-    def item_pickup():
-        pass
-        #entity_inv.append = item
+        if("W" in self._player_room and "E" in self._player_room + [1,0] ):
+            pass
+
+        if("N" in self._player_room and "S" in self._player_room + [0,1] ):
+            pass
+
+        if("S" in self._player_room and "N" in self._player_room + [0,-1] ):
+            pass
+
+        else:
+            print("You are trapped")
+        
     
-
-room1 = Room("Entry Hall", "", 1, 0, 0, 0, 1)
-
-def start_room():
-    setRoom_player.room1 = 1
