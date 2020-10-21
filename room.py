5007cd286ac1dc3a84dc38c162ef6202c7c4714f
Looter98 <35749173+Looter98@users.noreply.github.com>
2020-10-22 09:54:53 +1300

    Storing this method to do room based nav

--- room.py
+++ room.py
@@ -3,28 +3,28 @@ class Rooms:
     room_desc = ""
     room_position = []
     room_links = []
-    room_player = []
     #room_chest = #Calls a chest to appear in the room 
     #room_fight = #Calls a fight to play out when you enter the room
     
 
-    def __init__ (room_name, room_desc):
+    def __init__ (room_name, room_desc, room_position, room_links):
         self._room_name = room_name
         self._room_desc = room_desc
         self._room_position = room_position
         self._room_links = room_links
 
     def player_travel():
-        if("N" in self._player_room and "S" in self._player_room + [0,1] ):
+        if("N" in self_room_links ):
             pass
 
-        if("W" in self._player_room and "E" in self._player_room + [1,0] ):
+        if("W" in self._room_links ):
+            pass
+        
+
+        if("E" in self._room_links ):
             pass
 
-        if("N" in self._player_room and "S" in self._player_room + [0,1] ):
-            pass
-
-        if("S" in self._player_room and "N" in self._player_room + [0,-1] ):
+        if("S" in self._room_links ):
             pass
 
         else:
