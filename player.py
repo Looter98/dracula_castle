3f3eae118245dce363033c906fd4d59ebcab8217
Looter98 <35749173+Looter98@users.noreply.github.com>
2020-10-29 10:41:27 +1300

    Removed unneeded types of attack and defense

--- player.py
+++ player.py
@@ -1,28 +1,26 @@
 class Entity:
-        #GENERAL
-        entity_name = ""
-        entity_species = ""
+    #GENERAL
+    entity_name = ""
+    entity_species = ""
 
-        #DEFENSE
-        entity_health = 0
-        entity_defense = 0
-        entity_fire_defense = 0 #defense against fire attacks
-        entity_poison_defense = 0 #defense against poison attacks
+    #DEFENSE
+    entity_health = 0 #eneity's health in battle
+    entity_defense = 0 #base defense (increased by armour being worn)
 
-        #ATTACK
-        entity_short_attack = 0 #melee attacks
-        entity_long_attack = 0 #ranged attacks
-        entity_fire_attack = 0 #fire-based attacks
-        entity_poison_attack = 0 #poison-based attacks
+    #ATTACK
+    entity_attack = 0 #attacks (increasecd by weapons)
+
+    #SKILLS
+    entity_str = 0 #strength
+    entity_dex = 0 #dexterity (agility)
+    entity_per = 0 #persuasion
+    entity_int = 0 #intellegence/perception
+    entity_lck = 0 #luck
+
+    #Inventory
+    entity_inv = []
         
-        #SKILLS
-        entity_str = 0 #strength
-        entity_dex = 0 #dexterity (agility)
-        entity_per = 0 #persuasion
-        entity_int = 0 #intellegence/perception
-        entity_lck = 0 #luck
-        
-    def __init__(self, entity_name, entity_species, entity_str, entity_dex, entity_per, entity_int, entity_lck, entity_long_attack, entity_short_attack, entity_defense, entity_fire_defense, entity_poison_defense, entity_fire_attack, entity_poison_attack, entity_str, entity_dex, entity_per, entity_int, entity_lck): #player character object
+    def __init__(self, entity_name, entity_species, entity_str, entity_dex, entity_per, entity_int, entity_lck): #player character object
         self._entity_name = entity_name
         self._entity_species = entity_species
         self._entity_str = entity_str
@@ -30,14 +28,12 @@ class Entity:
         self._entity_per = entity_per
         self._entity_int = entity_int
         self._entity_lck = entity_lck
-        self._entity_long_attack = entity_long_attack
-        self._entity_short_attack = entity_short_attack
-        self._entity_fire_attack = entity_fire_attack
-        self._entity_poison_attack = entity_poison_attack
+        self._entity_attack = entity_attack
+        self._entity_health = entity_health 
         self._entity_defense = entity_defense
-        self._entity_fire_defense = entity_fire_defense
-        self._entity_poison_defense = entity_poison_defense
 
+
+    #Getters and Setters
     #GENERAL
     def getEntity_name( self ):
         return self._entity_name
@@ -64,42 +60,12 @@ class Entity:
     def setEntity_defense( self, entity_defense ):
         self._entity_defense = entity_defense
 
-    def getEntity_fire_defense ( self ):
-        return self._entity_fire_defense 
-
-    def setEntity_fire_defense ( self, entity_fire_defense ):
-        self._entity_fire_defense  = entity_fire_defense
-        
-    def getEntity_poison_defense ( self ):
-        return self._entity_poison_defense
-
-    def setEntity_poison_defense ( self, entity_poison_defense ):
-        self._entity_poison_defense  = entity_poison_defense
-
     #ATTACK
-    def getEntity_short_attack( self ):
-        return self._entity_short_attack
+    def getEntityt_attack( self ):
+        return self._entity_attack
                             
-    def setEntity_short_attack( self, entity_short_attack ):
-        self._entity_short_attack = entity_short_attack
-
-    def getEntity_long_attack( self ):
-        return self._entity_long_attack
-                            
-    def setEntity_long_attack( self, entity_long_attack ):
-        self._entity_long_attack = entity_long_attack
-
-    def getEntity_fire_attack ( self ):
-        return self._entity_fire_attack 
-
-    def setEntity_fire_attack ( self, entity_fire_attack ):
-        self._entity_fire_attack = entity_fire_attack
-        
-    def getEntity_poison_attack( self ):
-        return self._entity_poison_attack
-
-    def setEntity_poison_attack ( self, entity_poison_attack ):
-        self._entity_poison_attack = entity_poison_attack
+    def setEntity_attack( self, entity_attack ):
+        self._entity_attack = entity_attack
         
     #SKILLS
     def getEntity_str( self ):
@@ -132,11 +98,3 @@ class Entity:
     def setEntity_lck ( self, entity_lck ):
         self._entity_lck  = entity_lck
         
-                
-    
-
-            
-
-        
-        
-    
